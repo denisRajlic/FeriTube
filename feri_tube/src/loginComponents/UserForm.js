fdc193f25faf4490ca1a1bfa6a92058810dada21
DenisR <denis.rajlic@gmail.com>
2020-03-31 10:31:20 +0200

    Add Success message

--- feri_tube/src/loginComponents/UserForm.js
+++ feri_tube/src/loginComponents/UserForm.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import FormUserDetails from './FormUserDetails';
 import FormPersonalDetails from './FormPersonalDetails';
 import Confirm from './Confirm';
+import Success from './Success'
 
 const UserForm = () => {
 
@@ -74,7 +75,7 @@ const UserForm = () => {
         />
       )
     case 4:
-      return <h1>Success</h1>
+      return <Success />
     default:
       return <h1>Ups! Not handled properly</h1>
   }
