da948424deef91519d06cb15a050f38988282ea4
jasak17 <jasa.ksela@student.um.si>
2020-04-01 13:36:06 +0200

    Zacetek CourseCarda

--- feri_tube/src/App.js
+++ feri_tube/src/App.js
@@ -1,19 +1,44 @@
-import React from 'react';
-import './App.css';
-import UserForm from './loginComponents/UserForm';
-import Navigation from './Navigation/Navigation';
-import {Route} from 'react-router-dom'
-import HomePage from './HomePage/HomePage'
+import "./App.css";
+import UserForm from "./loginComponents/UserForm";
+import Navigation from "./Navigation/Navigation";
+import { Route } from "react-router-dom";
+import HomePage from "./HomePage/HomePage";
+import React, { Component } from "react";
 
+class App extends Component {
+  state = {
+    courses: [ //tetstni podatki
+      {
+        name: "Python",
+        opis: "",
+        dolzina: 5
+      },
+      {
+        name: "Assembler",
+        opis: "",
+        dolzina: 5
+      },
+      {
+        name: "Fittnes",
+        opis: "",
+        dolzina: 5
+      }
+    ]
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Route path="/register" exact component = {UserForm} />
-      <Route path="/" exact component = {HomePage} />
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Route path="/register" exact component={UserForm} />
+        <Route
+          path="/"
+          exact
+          render={props => <HomePage {...props} podatki={this.state.courses} />}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- feri_tube/src/HomePage/Jumbotron/Jumbotron.js
+++ feri_tube/src/HomePage/Jumbotron/Jumbotron.js
@@ -1,22 +1,33 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
 const Jumbotron = props => {
   return (
     <div>
-      <Card style={{backgroundColor: "#faf9f9"}}>
+      <Card style={{ backgroundColor: "#faf9f9" }}>
         <CardActionArea>
-          <CardMedia style={{height: 300, width: 350, marginLeft: "auto", marginRight: "auto", marginBottom: -90, marginTop: -90 }} image={require("../../Assets/logo4.png")} />
+          <CardMedia
+            style={{
+              height: 300,
+              width: 350,
+              marginLeft: "auto",
+              marginRight: "auto",
+              marginBottom: -90,
+              marginTop: -90
+            }}
+            image={require("../../Assets/logo4.png")}
+          />
           <CardContent>
-
-            <Typography variant="body2" color="textPrimary" component="p" align="center">
+            <Typography
+              variant="body2"
+              color="textPrimary"
+              component="p"
+              align="center"
+            >
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
